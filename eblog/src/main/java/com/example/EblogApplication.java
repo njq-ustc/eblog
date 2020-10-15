6073aba38dae5ff7f07ff8452238dc32f40aaecc
njq-ustc <596638515@qq.com>
2020-10-15 17:36:44 +0800

    v1.2

--- eblog/src/main/java/com/example/EblogApplication.java
+++ eblog/src/main/java/com/example/EblogApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EblogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EblogApplication.class, args);
         System.out.println("http://localhost:8080");
---
+++ eblog/src/main/java/com/example/template/TimeAgoMethod.java
@@ -0,0 +1,16 @@
+package com.example.template;
+
+import com.example.common.templates.DirectiveHandler;
+import com.example.common.templates.TemplateDirective;
+import freemarker.core.Environment;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateDirectiveModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class TimeAgoMethod implements TemplateDirective {
+
+}
